7c9b3d487b5d1ad244a2ff469de29f5188234750
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-07-14 10:59:27 +0700

    add validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const { validationResult } = require('express-validator');
+
 exports.index = (req, res, next) => {
     res.status(200).json({message : "act"});
 }
@@ -6,10 +8,19 @@ exports.register =async (req, res, next) => {
     try{
         const { name, email, password} = req.body;
 
+        //validation
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error('format invalid');
+            error.statusCode = 422;
+            error.validation = errors.array();
+            throw error;
+        }
+
         //check email
         const existEmail = await User.findOne({email : email});
         if(existEmail){
-            const error = new Error('email ซ้ำ');
+            const error = new Error('email repeat');
             error.statusCode = 400;
             throw error;
         }
@@ -23,6 +34,26 @@ exports.register =async (req, res, next) => {
         next(error);
     }
 }
-exports.login = (req, res, next) => {
-    res.status(200).json({message : "login"});
+exports.login =async (req, res, next) => {
+    try{
+        const { email, password} = req.body;
+
+        //check email exist system
+        const user = await User.findOne({email : email});
+        if(!user){
+            const error = new Error('not found user');
+            error.statusCode = 404;
+            throw error;
+        }
+        // compare password ถ้าไม่ตรง Return false
+        const isValid = await user.checkPassword(password);
+        if(!isValid){
+            const error = new Error('password invalid');
+            error.statusCode = 401;
+            throw error;
+        }
+        res.status(200).json({message : "login success"});
+    }catch(error){
+        next(error);
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { body} = require('express-validator');
 const router = express.Router();
 const userController = require('../controllers/userController')
 /* GET users listing. */
 router.get('/', userController.index);
-router.get('/login', userController.login);
-router.post('/register', userController.register);
+
+router.post('/login', userController.login);
+
+router.post('/register', [
+    body('name').not().isEmpty().withMessage('please insert name'),
+    body('email').not().isEmpty().withMessage('please insert email').isEmail().withMessage('email format invalid'),
+    body('password').not().isEmpty().withMessage('please insert password').isLength({min:8}).withMessage('password length 8')
+], userController.register);
 
 module.exports = router;
