78c5f0eb850f2920f371a54596033c9cd5e25b94
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-08-04 21:55:52 +0700

    setting format user.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body} = require('express-validator');
+const { body } = require('express-validator');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const passportJWT = require('../middleware/passportJWT');
